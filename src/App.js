3a1c20ee5cf84e23e4042c205e33e7cb40ac1b6d
auk <l202199@lhr.nu.edu.pk>
2023-03-16 19:23:45 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+import { Configuration , OpenAIApi} from "openai"
+
 
 function App() {
+
+  const [prompt , setPrompt] = useState("")
+  const [result , setResult] = useState("")
+
+  const configuration = new Configuration({
+    apiKey:"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"
+
+  })
+  
+  const openai = new OpenAIApi(configuration);
+  const genrateImage = async() =>{
+    const res = await openai.createImage({
+      prompt: prompt , 
+      n: 1,
+      size: "512x512"
+      //prompt
+      //n 1 - 10
+      //size 1024 x 1024 default $0.02
+      //respose_format
+      //user
+    })
+    
+    setResult(res.data.data[0].url);
+    //console.log(res);
+  }
+
+  console.log(prompt)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>genrate image</h2>
+      <br/>
+      <textarea
+       placeholder='lets genrate image'
+       onChange={(e) => setPrompt(e.target.value)} 
+       />
+
+      <img src={logo} className="App-logo" alt="logo" />
+        
+      <br/>
+      <button onClick ={genrateImage}>genrateimage</button>
+      <hr/>
+      
+      <img src = {result} alt  = {result} />
+    
+      
+        
     </div>
   );
 }
